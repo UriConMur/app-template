c462dc460aa876793f61e51843a065943a49078a
Uriel Contreras <uriel.contreras@beliveo.com>
2018-01-10 13:01:56 -0600

    Remove Doc files PHPH

--- src/server/src/config/controller.php
+++ src/server/src/config/controller.php
@@ -1,15 +1,4 @@
 <?php
-/**
- * PHP Version 5
- *
- * Controller Doc Comment
- *
- * @category File
- * @package  Controller
- * @author   Salvador Gonzalez <salvador.gonzalez@beliveo.com>
- * @license  https://github.com/SDBLV/bbox-login/blob/develop/LICENSE.md MIT
- * @link     http://www.beliveo.net/bbox-login
- */
 
 /**
  * Function getDBData
--- src/server/src/config/db.php
+++ src/server/src/config/db.php
@@ -1,17 +1,5 @@
 <?php
 /**
- * PHP Version 5
- *
- * DB Doc Comment
- *
- * @category File
- * @package  DB
- * @author   Salvador Gonzalez <salvador.gonzalez@beliveo.com>
- * @license  https://github.com/SDBLV/bbox-login/blob/develop/LICENSE.md MIT
- * @link     http://www.beliveo.net/bbox-login
- */
-
-/**
  * DataBase
  *
  * @category   Class
