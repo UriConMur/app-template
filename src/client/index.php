640128fad8be2bdedc8432c251ab8705f8481a29
Uriel Contreras <uriel.contreras@beliveo.com>
2018-01-15 16:56:37 -0600

    Add styles for component imported

--- src/client/index.php
+++ src/client/index.php
@@ -1,14 +1,28 @@
-<?php
-require '../../config/environment.php';
-
-$cookie_name = 'unou';
-if (! isset($_COOKIE[ $cookie_name ])) {
-    header("location: {$_ENV['LOGIN_URL']}?redirect_to={$_ENV['DOMAIN']}/app-template");
-} else {
-    if (isset($_GET['p'])) {
-        $redirect = $_GET['p'];
-        include "pages/{$redirect}/index.html";
+<!DOCTYPE html>
+<html>
+<head>
+<title>Beliveo</title>
+  <link rel="stylesheet" type="text/css" href="src/client/styles/main.css">
+</head>
+<body>
+<?php include_once '../../node_modules/beliveo-components/components/header.php'; ?>
+<div class="body_container">
+  <?php
+    require '../../config/environment.php';
+    $cookie_name = 'unou';
+    if (! isset($_COOKIE[ $cookie_name ])) {
+        header("location: {$_ENV['LOGIN_URL']}?redirect_to={$_ENV['DOMAIN']}/app-template");
     } else {
-        include 'pages/home/index.html';
+        if (isset($_GET['p'])) {
+            $redirect = $_GET['p'];
+            include "pages/{$redirect}/index.html";
+        } else {
+            include 'pages/home/index.html';
+        }
     }
-}
+  ?>
+</div>
+
+<?php include_once '../../node_modules/beliveo-components/components/footer.php'; ?>
+</body>
+</html>
