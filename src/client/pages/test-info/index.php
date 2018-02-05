e9f904fe6311ca44f7d113924c993437d9cd69ed
Uriel Contreras <uriel.contreras@beliveo.com>
2018-02-04 20:54:46 -0600

    Refactor structure and remove generic code

---
+++ src/client/pages/test-info/index.php
@@ -0,0 +1,13 @@
+<head>
+	<title>Beliveo bBox - Powered by people</title>
+	<link rel="stylesheet" type="text/css" href="src/client/pages/test-info/css/styles.css">
+	<script src="node_modules/jquery/dist/jquery.js"></script>
+</head>
+<div class="test-info">
+		<h1>Test Info</h1>
+			<center>
+				<h2>SESSION</h2>
+				<?php var_dump($_SESSION); ?>
+            </center>
+        </div>
+</div>
