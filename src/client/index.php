fa364d73fd8b13a7db07e9d2e3ed288b73155230
Uriel Contreras <uriel.contreras@beliveo.com>
2018-02-24 15:28:19 -0600

    Add side bar manu to index file

--- src/client/index.php
+++ src/client/index.php
@@ -3,6 +3,7 @@
 <head>
 <title>Beliveo</title>
   <script src="node_modules/beliveo-files/utils/urlFix/index.js"></script>
+  <link rel="stylesheet" type="text/css" href="node_modules/bootstrap/dist/css/bootstrap.css">
   <link rel="stylesheet" type="text/css" href="node_modules/beliveo-files/styles/main.css">
 </head>
 <body>
@@ -13,21 +14,28 @@ if (session_status() == PHP_SESSION_NONE) {
 require '../../config/environment.php';
 ?>
 <?php require_once '../../node_modules/beliveo-files/components/header.php'; ?>
-<div class="body_container">
-    <?php
-    $cookie_name = 'unou';
-    if (! isset($_COOKIE[ $cookie_name ])) {
-        session_destroy();
-        header("location: {$_ENV['LOGIN_URL']}?redirect_to={$_ENV['DOMAIN']}/{$_ENV['NAME_APP']}");
-    } else {
-        if (isset($_GET['p'])) {
-            $redirect = $_GET['p'];
-            include "pages/{$redirect}/index.php";
-        } else {
-            include 'pages/home/index.php';
-        }
-    }
-    ?>
+<div class='row'>
+  <div id='sideMenu' class="col col-md-2">
+    <?php require_once '../../node_modules/beliveo-files/components/sideMenu.php'; ?>
+  </div>
+  <div class="body_container col col-md-10">
+    <div class='content'>
+      <?php
+      $cookie_name = 'unou';
+      if (! isset($_COOKIE[ $cookie_name ])) {
+          session_destroy();
+          header("location: {$_ENV['LOGIN_URL']}?redirect_to={$_ENV['DOMAIN']}/{$_ENV['NAME_APP']}");
+      } else {
+          if (isset($_GET['p'])) {
+              $redirect = $_GET['p'];
+              include "pages/{$redirect}/index.php";
+          } else {
+              include 'pages/home/index.php';
+          }
+      }
+      ?>
+    </div>
+  </div>
 </div>
 <?php require_once '../../node_modules/beliveo-files/components/footer.php'; ?>
 </body>
